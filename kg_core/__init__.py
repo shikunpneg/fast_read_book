fb0592b9e2f3fdd11c670e6d8ccd5b4ce66b71fc
DDLC(dog dog love cat) <145889015+shikunpneg@users.noreply.github.com>
2026-06-29 15:59:38 +0800

    release: v1.0.1

--- kg_core/__init__.py
+++ kg_core/__init__.py
@@ -6,5 +6,5 @@ from .builder import KnowledgeGraphBuilder
 from .summarizer import SummaryGenerator
 from .text_cleaner import clean_text, split_chapters
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["KnowledgeGraphBuilder", "SummaryGenerator", "clean_text", "split_chapters"]
